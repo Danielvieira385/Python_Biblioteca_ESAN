35544df343752d3977f21fd5db99d198a8945d70
Danielvieira385 <danielvieira385@gmail.com>
2024-11-26 13:47:53 +0000

    Updates 1.2

--- delete_users.py
+++ delete_users.py
@@ -5,19 +5,23 @@ from datetime import datetime
 
 #### código ainda em construção - DV
 
-def get_user():
-    # nome = input('Indique o nome do Utilizador que pretende remover: ')
+def getUserToRemove():
+    utilizador = input('Indique o Utilizador que pretende remover: ')
     
-    #      # Colocar a confirmação de ter a certeza para eliminar
+    infoUserToRemove = []
 
+    with open(dirs.dirUsers, 'r') as file:
+        listaUsers = file.readline()
+
+        for nome in listaUsers:
+            if nome == utilizador:
+                infoUserToRemove.append[utilizador]
+ 
     # if dirs.passAdmin == getpass.getpass('Introduza a password de administrador: ', '*'):
-
-    # user_info = []
-
-    # with open(dirs.dirUsers, 'r') as file:
-    #     lista = csv.DictReader(file, fieldnames=['nome', 'password'])
-
-    #     for row in lista:
-    #         user_info.pop(row)
-
-    # return user_info
+    #     for utilizador in 
+ 
+    return infoUserToRemove
+    
+    
+i = getUserToRemove()
+print(i)
