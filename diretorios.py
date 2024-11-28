efaa08bf875627c90ae6ad81e9784a9bf1f1014f
Danielvieira385 <danielvieira385@gmail.com>
2024-11-28 00:57:35 +0000

    update

--- diretorios.py
+++ diretorios.py
@@ -1,5 +1,4 @@
 dirBooks = "Danielvieira385/Python_Biblioteca_ESAN/books_DB.csv"
-# dirUsers = "Danielvieira385/Python_Biblioteca_ESAN/users_DB.csv"
-dirUsers = "users_DB.csv"
+dirUsers = "Danielvieira385/Python_Biblioteca_ESAN/users_DB.csv"
 
 passAdmin = "0000"
