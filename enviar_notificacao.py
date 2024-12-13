951b9cfa710d449a586525a0f825f8844dde244e
Danielvieira385 <danielvieira385@gmail.com>
2024-12-13 14:49:16 +0000

    Função de envio de email para usar variável de ambiente para senha e adiciona logging de erros

--- enviar_notificacao.py
+++ enviar_notificacao.py
@@ -1,40 +1,42 @@
-import smtplib, ssl 
+import os
+import smtplib
+import ssl
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+import logging
 
-### Ainda em desenvolvimento ###
+# Configure logging
+logging.basicConfig(level=logging.ERROR, format='%(asctime)s %(levelname)s:%(message)s')
 
 def enviar_email(destinatario, assunto, mensagem):
     remetente = 'biblioteca.estarreja.py@gmail.com'
     smtp_server = "smtp.gmail.com"
     port = 587  # For starttls
     sender_email = "biblioteca.estarreja.py@gmail.com"
-    password = '.123456789.'
+    password = os.getenv('EMAIL_PASSWORD')  # Use environment variable for password
 
     # Create a secure SSL context
     context = ssl.create_default_context()
 
-    # Try to log in to server and send email
     try:
-        server = smtplib.SMTP(smtp_server,port)
-        server.ehlo() # Can be omitted
-        server.starttls(context=context) # Secure the connection
-        server.ehlo() # Can be omitted
+        server = smtplib.SMTP(smtp_server, port)
+        server.ehlo()
+        server.starttls(context=context)  # Secure the connection
         server.login(sender_email, password)
         msg = MIMEMultipart()
         msg['From'] = remetente
         msg['To'] = destinatario
         msg['Subject'] = assunto
         msg.attach(MIMEText(mensagem, 'plain'))
-        server.sendmail(remetente, destinatario, msg.as_string())
-    except Exception as e:
-        # Print any error messages to stdout
-        print(e)
+        server.sendmail(sender_email, destinatario, msg.as_string())
+    except Exception as event:
+        logging.error(f"Erro ao enviar email: {event}")
     finally:
         server.quit()
 
-# Exemplo de uso
-destinatario = 'biblioteca.estarreja.py@gmail.com'
-assunto = 'Assunto do Email'
-mensagem = 'Corpo do email'
-enviar_email(destinatario, assunto, mensagem)
+if __name__ == "__main__":
+    # Exemplo de uso
+    destinatario = 'biblioteca.estarreja.py@gmail.com'
+    assunto = 'Assunto do Email'
+    mensagem = 'Corpo do email'
+    enviar_email(destinatario, assunto, mensagem)
