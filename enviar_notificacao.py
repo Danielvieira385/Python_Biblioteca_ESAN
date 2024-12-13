25dcad40e7624f68c7841bd046c4ce9829676a14
Danielvieira385 <danielvieira385@gmail.com>
2024-12-13 15:26:30 +0000

    Função de envio de e-mail para simplificar a implementação

--- enviar_notificacao.py
+++ enviar_notificacao.py
@@ -1,40 +1,14 @@
-import os
-import smtplib
-import ssl
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-import logging
+from smtplib import SMTP
 
-def enviar_email(destinatarios, assunto, mensagem):
-    remetente = 'biblioteca.estarreja.py@gmail.com'
-    smtp_server = "smtp.gmail.com"
-    port = 587 
-    sender_email = "biblioteca.estarreja.py@gmail.com"
-    password = '.123456789.'
-    
-    # Create a secure SSL context
-    context = ssl.create_default_context()
-
-    try:
-        server = smtplib.SMTP(smtp_server, port)
-        server.ehlo()
-        server.starttls(context=context)  # Secure the connection
-        server.login(sender_email, password)
-        msg = MIMEMultipart()
-        msg['From'] = remetente
-        msg['To'] = ", ".join(destinatarios)
-        msg['Subject'] = assunto
-        msg.attach(MIMEText(mensagem, 'plain'))
-        server.sendmail(sender_email, destinatarios, msg.as_string())
-    except (smtplib.SMTPException, ssl.SSLError) as event:
-        logging.error(f"Erro ao enviar email para {destinatarios}: {event}")
-    finally:
-        server.quit()
-
-if __name__ == "__main__":
-    # Exemplo de uso
-    # Example usage for testing purposes only. Do not use hardcoded credentials in production.
-    destinatarios = ['biblioteca.estarreja.py@gmail.com']
-    assunto = 'Assunto do Email'
-    mensagem = 'Corpo do email'
-    enviar_email(destinatarios, assunto, mensagem)
+# Função para enviar e-mail
+def enviar_email(usuario,senha,mensagem,listadestinatarios):
+    smtp = SMTP('smtp.gmail.com',587)
+    smtp.starttls()
+    smtp.login(usuario,senha)
+    smtp.sendmail(usuario,listadestinatarios,mensagem)
+    smtp.quit()
+    print('E-mail enviado com sucesso')
+enviar_email('biblioteca.estarreja.py','.123456789.','ola mundo',['biblioteca.estarreja.py@gmail.com'])
+ 
+ 
+ 
